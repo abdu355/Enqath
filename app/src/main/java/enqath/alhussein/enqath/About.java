a4dbc447bbbca821795ebaa2dc71137f8bc3fea0
abdu355 <abdu_sah@hotmail.com>
2016-07-06 13:45:30 +0400

    v1.0.5

--- app/src/main/java/enqath/alhussein/enqath/About.java
+++ app/src/main/java/enqath/alhussein/enqath/About.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class About extends AppCompatActivity {
-
+//add coordinator layout
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
