8e7ba7e5a6205ee889193afab991ce3d5fe62772
abdu355 <abdu_sah@hotmail.com>
2016-07-06 13:36:04 +0400

    v1.0.5
    -Permission Fixed
    -Settings Activity Fixed

--- app/src/main/java/enqath/alhussein/enqath/Settings.java
+++ app/src/main/java/enqath/alhussein/enqath/Settings.java
@@ -19,11 +19,12 @@ public class Settings extends Activity implements View.OnClickListener, AdapterV
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
 
         save=(Button)findViewById(R.id.btnSave);
         save.setOnClickListener(this);
 
-        setContentView(R.layout.settings);
+
         R1=(TableRow)findViewById(R.id.R1);
         R2=(TableRow)findViewById(R.id.R2);
         R3=(TableRow)findViewById(R.id.R3);
